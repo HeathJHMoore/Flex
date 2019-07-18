34458db0b22e67a4074d2a48d23b0efc0d8db847
Heath JH Moore <heathjhmoore@gmail.com>
2019-07-17 19:23:54 -0500

    set up routes

---
+++ src/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <p>This is Auth</p>
+    )
+  }
+}
+
+export default Auth;
---
+++ src/ExerciseDictionary/ExerciseDictionary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ExerciseDictionary extends React.Component {
+  render() {
+    return (
+      <p>This is Exercise Dictionary</p>
+    )
+  }
+}
+
+export default ExerciseDictionary;
