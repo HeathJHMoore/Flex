4dcbac4a9fa3b6327364eef1aca0fc37381e6a4f
Heath JH Moore <heathjhmoore@gmail.com>
2019-07-27 11:58:41 -0500

    finished

--- src/components/SubmitWorkout/SubmitWorkout.js
+++ src/components/SubmitWorkout/SubmitWorkout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import exerciseData from '../../helpers/data/exerciseData';
+
 class SubmitWorkout extends React.Component {
   state = {
     currentWorkout : this.props.match.params.workoutId
   }
-  
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <h1>{this.state.currentWorkout}</h1>
