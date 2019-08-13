297c209cec6ab6a56a652dfd6341d960ccdf56d7
Heath JH Moore <heathjhmoore@gmail.com>
2019-08-12 20:44:26 -0500

    got filtering working and added styles

--- src/components/ExerciseDictionary/ExerciseDictionary.js
+++ src/components/ExerciseDictionary/ExerciseDictionary.js
@@ -4,6 +4,8 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
 import exerciseData from '../../helpers/data/exerciseData';
 import ExerciseCard from '../ExerciseCard/ExerciseCard';
 
+import './ExerciseDictionary.scss';
+
 class ExerciseDictionary extends React.Component {
 
   state = {
@@ -22,20 +24,29 @@ class ExerciseDictionary extends React.Component {
 
   componentDidMount() {
     exerciseData.getExercises()
-      .then((resp) => this.setState({exercises : resp}))
+      .then((resp) => {
+        this.setState({allExercises : resp, shownExercises : resp})
+      })
       .catch(console.error('couldnt get exercises'))
   }
 
   filterExercisesBySearch = (e) => {
-    const search = e.target.value
+    const search = e.target.value;
+    console.error(search);
     const newExercises = [];
-    this.state.exercises.forEach((exercise) => {
-      if (exercise.name == search) {
+    if (search === null) {
+      this.setState({shownExercises : this.state.allExercises})
+    } else {
+      this.state.allExercises.forEach((exercise) => {
+        if (exercise.name.toLowerCase().includes(search.toLowerCase())) {
+        console.error('successful match!!!!')
         newExercises.push(exercise)
-      }
-    })
-    this.setState({exercises : newExercises})
+        }
+      })
+    }
+    this.setState({shownExercises : newExercises})
   }
+
 
   muscleDropdownToggle = () => {
     this.setState({muscleDropdownToggle : !this.state.muscleDropdownToggle})
@@ -53,24 +64,25 @@ class ExerciseDictionary extends React.Component {
   }
 
   render() {
-    const exerciseCards = this.state.exercises.map((exercise) => (
+    const exerciseCards = this.state.shownExercises.map((exercise) => (
       <ExerciseCard exercise={exercise}/>
     ))
-    const filteredExercises = () => this.state.muscleList.map((muscle) => (
-      <DropdownItem onClick={this.chooseMuscle}></DropdownItem>
+    const muscleChoices = this.state.muscleList.map((muscle) => (
+      <DropdownItem onClick={this.chooseMuscle}>{muscle}</DropdownItem>
     ))
     return (
       <div className="col-12 mt-2">
-        <div className="row justify-content-center mt-2 mb-4">
-          <input type="text" placeholder="Search Exercise By Name" className="col-8 col-sm-5 col-lg-4 text-center createNewWorkoutborder" onChange={this.filterExercisesBySearch}></input>
+        <div className="row justify-content-center mt-2 mb-4 searchExerciseContainer">
+          <input type="text" placeholder="Search Exercise By Name" className="col-8 col-sm-6 col-lg-4 text-center createNewWorkoutborder" onChange={this.filterExercisesBySearch}></input>
+          <i class="fas fa-search"></i>
         </div>
         <div className="row justify-content-center mt-2 mb-4">
-          <Dropdown isOpen={this.state.muscleDropdownToggle} toggle={this.toggleMuscleDropdown} className="statisticDropdown">
+          <Dropdown isOpen={this.state.muscleDropdownToggle} toggle={this.muscleDropdownToggle} className="statisticDropdown">
             <DropdownToggle caret>
               Search By Muscle Group
             </DropdownToggle>
             <DropdownMenu>
-              {filteredExercises}
+              {muscleChoices}
             </DropdownMenu>
           </Dropdown>
         </div>
